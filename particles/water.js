8cfd2d97b234820d5394a72df112ffa5ee7f2408
Nate Buel <natebuel29@gmail.com>
2023-11-24 16:40:24 -0600

    water puts out fire - this be ugly

--- particles/water.js
+++ particles/water.js
@@ -13,24 +13,37 @@ class Water extends Particle {
         if (gameCells.getCell(x, y, 0, 1) === 0) {
             gameCells.swapCells(x, y, 0, 1);
         }
+        else if (gameCells.getCell(x, y, 0, 1) !== 0 && gameCells.getCell(x, y, 0, 1).type === 'fire') {
+            gameCells.setCell(x, y, 0, 1, 0)
+        }
         else if (gameCells.getCell(x, y, 1, 1) === 0 && gameCells.getCell(x, y, -1, 1) === 0) {
             var randomDirection = Math.random() > 0.5 ? 1 : -1;
             gameCells.swapCells(x, y, randomDirection, 1);
+        }
+        else if (gameCells.getCell(x, y, 1, 1) !== 0 && gameCells.getCell(x, y, 1, 1).type === 'fire') {
+            gameCells.setCell(x, y, 1, 1, 0)
+        }
 
+        else if (gameCells.getCell(x, y, -1, 1) === 0 && gameCells.getCell(x, y, -1, 1).type === 'fire') {
+            gameCells.setCell(x, y, -1, 1, 0)
         }
         else if (gameCells.getCell(x, y, 1, 0) === 0 && gameCells.getCell(x, y, -1, 0) === 0) {
             var randomDirection = Math.random() > 0.5 ? 1 : -1;
             gameCells.swapCells(x, y, randomDirection, 0);
-
         }
         // move left
         else if (gameCells.getCell(x, y, -1, 0) === 0) {
             gameCells.swapCells(x, y, -1, 0);
-
+        }
+        else if (gameCells.getCell(x, y, -1, 0) !== 0 && gameCells.getCell(x, y, -1, 0).type === 'fire') {
+            gameCells.setCell(x, y, -1, 0, 0)
         }
         //move right
         else if (gameCells.getCell(x, y, 1, 0) === 0) {
             gameCells.swapCells(x, y, 1, 0);
+        }
+        else if (gameCells.getCell(x, y, 1, 0) !== 0 && gameCells.getCell(x, y, 1, 0).type === 'fire') {
+            gameCells.setCell(x, y, 1, 0, 0)
         }
     }
 }
